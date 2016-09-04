ff2150b6cdeb5bb2b6d32d4c2733639a9e53dfd7
SURAJTHEGREAT <suraj.gate2013@gmail.com>
2016-09-04 18:18:17 +0000

    Docker change

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
 import resin_func
 print "Welcome to Resin"
 resin_func.default()
-print "Running this from docker component,to check data usage"
+print "Running this from docker component,to check data usage v2"
