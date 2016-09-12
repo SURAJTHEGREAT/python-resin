ba06621d3d8f68661b411873a283bfc591d43c0b
Suraj S <suraj.s@cognizant.com>
2016-09-12 13:12:31 +0000

    Changed iotivity file

--- iotivity.sh
+++ iotivity.sh
@@ -1,4 +1,8 @@
 #bin/sh
 set -vx
+echo "Trying to start docker"
 sudo service docker start
+echo "Started docker"
+sleep 10
+echo "Sleeping 10 secs"
 sudo docker pull surajthegreat/iotivity_ubuntu_final:latest
