2ad9f43f62b82b2aa7b5fbe682760249a31fa38a
Suraj S <suraj.s@cognizant.com>
2016-09-13 05:29:22 +0000

    Change docker file and iotivity file to lias with

--- iotivity.sh
+++ iotivity.sh
@@ -6,3 +6,5 @@ echo "Started docker"
 sleep 10
 echo "Sleeping 10 secs"
 sudo docker pull surajthegreat/iotivity_ubuntu_final:latest
+sleep 5
+java -jar /usr/local/quarksjava/Sample_Edgent/target/Sample_Edgent-0.0.1-SNAPSHOT.jar
